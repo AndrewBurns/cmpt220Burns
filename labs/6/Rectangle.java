346b0d8f95418cb8458a3e5a8500d75102c0b0bb
AndrewBurns <andrew.burns4@marist.edu>
2016-10-20 22:45:20 -0400

    lab 6 subit

--- labs/6/Rectangle.java
+++ labs/6/Rectangle.java
@@ -1,4 +1,3 @@
-
 /**
  * file: Rectangle.java
  * author: Drew Burns
@@ -14,43 +13,41 @@
 import java.util.Scanner;
 
 public class Rectangle {
-  // main method
-  // create 2 double fields height and weight default 1
+	// main method
+	// create 2 double fields height and weight default 1
 
-  double length = 1;
-  double width = 1;
+	double length = 1;
+	double width = 1;
 
-  public static void main(String[] args) {
-    // create a rectangle with defaults
-    Rectangle Rectangle1 = new Rectangle();
-    System.out.println("the area of the rectangle of width " + 
-        Rectangle1.width + " and length of " + Rectangle1.length +
-            " is " + Rectangle1.getArea());
-    System.out.println("the perimeter of the rectangle of width " + 
-        Rectangle1.width + " and length of "+ Rectangle1.length +
-            " is " + Rectangle1.getPerimeter());
+	public static void main(String[] args) {
+		// create a rectangle with defaults
+		Rectangle Rectangle1 = new Rectangle();
+		System.out.println("the area of the rectangle of width " + Rectangle1.width + " and length of "
+				+ Rectangle1.length + " is " + Rectangle1.getArea());
+		System.out.println("the perimeter of the rectangle of width " + Rectangle1.width + " and length of "
+				+ Rectangle1.length + " is " + Rectangle1.getPerimeter());
 
-  }
+	}
 
-  // no args constuctor that creates a default rectangle
-  Rectangle() {
+	// no args constuctor that creates a default rectangle
+	Rectangle() {
 
-  }
+	}
 
-  // a constructor that creates a rect with the specified width and height
-  Rectangle(int width, int length) {
-    // Get user input
-    this.length = length;
-    this.width = width;
-  }
+	// a constructor that creates a rect with the specified width and height
+	Rectangle(int width, int length) {
+		// Get user input
+		this.length = length;
+		this.width = width;
+	}
 
-  // method that returns the area of rec
-  double getArea() {
-    return this.length * this.width;
-  }
+	// method that returns the area of rec
+	double getArea() {
+		return this.length * this.width;
+	}
 
-  // method that returns the perimeter
-  double getPerimeter() {
-    return (2 * (this.length * this.width));
-  }
+	// method that returns the perimeter
+	double getPerimeter() {
+		return (2 * (this.length * this.width));
+	}
 }
