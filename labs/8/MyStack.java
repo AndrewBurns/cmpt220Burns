b4b6dbadbbce73d5a089906d5a8ff686753d0abb
AndrewBurns <andrew.burns4@marist.edu>
2016-11-04 16:11:57 -0400

    space

--- labs/8/MyStack.java
+++ labs/8/MyStack.java
@@ -12,12 +12,12 @@
 import java.util.ArrayList;
 
 public class MyStack {
-  
+
   private ArrayList<Object> list = new ArrayList<>();
   
   
   public boolean isEmpty() {
-  return list.isEmpty();
+    return list.isEmpty();
   }
   
   public int getSize(){
